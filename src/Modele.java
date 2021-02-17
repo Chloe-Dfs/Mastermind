9c9bdbbded09647377f04ca8855a88e828a645aa
Chloe <chloe.defosse.contact@gmail.com>
2021-02-17 11:40:38 +0100

    2eme et 3eme point ex1

--- src/Modele.java
+++ src/Modele.java
@@ -1,8 +1,9 @@
 
 public class Modele {
 	
+	
 	int N_TENTATIVES = 10;
-	int DIFFICULTE = 4;
+	static int DIFFICULTE = 4;
 	String [] COULEURS = { "Jaune", "Vert", "Bleu", "Magenta", "Rouge", "Orange", "Blanc", "Noir"};
 	public enum Etat {
 		EN_COURS, GAGNE, PERDU
@@ -11,5 +12,11 @@ public class Modele {
 	Etat enCours = Etat.EN_COURS;
 	Etat gagne = Etat.GAGNE;
 	Etat perdu = Etat.PERDU;
+	
+	Etat etat;
+	Range combinaison;
+	Range[] propositions;
+	int tentative;
+	//propositions[tentative]
 
 }
--- src/Range.java
+++ src/Range.java
@@ -1,4 +1,11 @@
 
 public class Range {
 
+	int[] jetons = new int[Modele.DIFFICULTE];
+	int indiceJeton = 0;
+	int noirs;
+	int blancs;
+	int[] resultat = {noirs,blancs};
+	
+	
 }
